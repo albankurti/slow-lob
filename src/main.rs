6c0dd9d41d2cf113558abcd4adeb6428203ff6c6
Alban Kurti <>
2023-01-31 14:53:38 +0100

    binance, upbit added, coinbase env variable func added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
-use std::env;
 mod websockets;
+mod exchange_traits;
+
+use std::env;
+use websockets::{BinanceBook, CoinbaseBook, UpbitBook};
+use exchange_traits::ExchangeFunctionality;
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "full");
+
+    let binance: BinanceBook = BinanceBook {
+        name: String::from("binance"),
+        base_ws: String::from("wss://ws-api.binance.com:443/ws-api/v3"),
+        symbol: String::from("BTCUSDT"),
+        method: String::from("depth"),
+        limit: 5,
+    };
+
+    // binance.subscribe();
+
+    let coinbase: CoinbaseBook = CoinbaseBook {
+        name: String::from("coinbase"),
+        base_ws: String::from("wss://advanced-trade-ws.coinbase.com"),
+        api_key: CoinbaseBook::get_api_key(),
+        secret: CoinbaseBook::get_api_secret(),
+        product_ids: vec![String::from("ETH-USD"), String::from("BTC-USD")],
+        channel: String::from("level2"),
+    };
+
+    coinbase.subscribe();
+
+    let upbit: UpbitBook = UpbitBook {
+        name: String::from("upbit"),
+        base_ws: String::from("wss://api.upbit.com/websocket/v1"),
+        channel: String::from("orderbook"),
+        codes: vec![String::from("USDT-BTC")],
+    };
+
+    // upbit.subscribe();
+
+
 }
--- src/websockets.rs
+++ src/websockets.rs
@@ -1,27 +1,23 @@
-use std::time;
+use std::{time,env};
 use sha2::{Sha256, Sha512, Digest};
 use hmac::{Hmac, Mac};
 use serde_json::json;
 use websocket::{client::builder::ClientBuilder, message::OwnedMessage, result::WebSocketError};
+use rand::{Rng, thread_rng, random};
+use rand::distributions::Alphanumeric;
+use crate::exchange_traits::ExchangeFunctionality;
 
-trait ExchangeFunc {
-    fn generate_hmac(&self) -> String;
-    fn subscribe(&self);
-    fn unsubscribe(&self);
-    fn parse_data(&self);
+pub struct CoinbaseBook {
+    pub name: String,
+    pub base_ws: String,
+    pub api_key: String,
+    pub secret: String,
+    pub product_ids: Vec<String>,
+    pub channel: String,
 }
 
-struct Coinbase {
-    name: String,
-    base_ws: String,
-    api_key: String,
-    secret: String,
-    product_ids: Vec<String>,
-    channel: String,
-}
-
-impl ExchangeFunc for Coinbase{
-    fn generate_hmac(&self) -> String {
+impl CoinbaseBook{
+    pub fn generate_hmac(&self) -> String {
         let timestamp = (time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().as_secs() as i64).to_string();
         let product_ids_string = &self.product_ids.join(",");
         let prehash: String = format!("{}{}{}", timestamp, &self.channel, product_ids_string);
@@ -34,6 +30,16 @@ impl ExchangeFunc for Coinbase{
         return hex::encode(&code_bytes.to_vec());
     }
 
+    pub fn get_api_key() -> String {
+        env::var("API_KEY").expect("API_KEY environment variable is not set")
+    }
+
+    pub fn get_api_secret() -> String {
+        env::var("API_SECRET").expect("API_SECRET environment variable is not set")
+    }
+}
+
+impl ExchangeFunctionality for CoinbaseBook{
     fn subscribe(&self){
         let mut websocket = ClientBuilder::new(&self.base_ws)
         .unwrap()
@@ -84,3 +90,139 @@ impl ExchangeFunc for Coinbase{
         //TODO
     }
 }
+
+pub struct BinanceBook{
+    pub name: String,
+    pub base_ws: String,
+    pub symbol: String,
+    pub method: String,
+    pub limit: u32,
+}
+
+impl ExchangeFunctionality for BinanceBook{
+    fn subscribe(&self) {
+        let mut websocket = ClientBuilder::new(&self.base_ws)
+        .unwrap()
+        .connect(None)
+        .expect("Failed to connect to websocket");
+
+        let mut rng = thread_rng();
+        let random_id: String = (0..16).map(|_| rng.sample(Alphanumeric) as char).collect();
+
+        let message = json!({
+            "id": random_id,
+            "method": &self.method,
+            "params": {
+                "symbol": &self.symbol,
+                "limit": &self.limit
+            },
+        });
+
+        websocket.send_message(&OwnedMessage::Text(message.to_string()))
+            .expect("Failed to send message");
+
+        loop {
+            let message = match websocket.recv_message() {
+                Ok(message) => message,
+                Err(e) => {
+                    println!("Error receiving message: {:?}", e);
+                    break;
+                }
+            };
+
+            match message {
+                OwnedMessage::Text(s) => println!("Text message received: {}", s),
+                OwnedMessage::Binary(b) => println!("Binary message received: {:?}", b),
+                OwnedMessage::Close(_) => {
+                    println!("Closing connection");
+                    break;
+                }
+                _ => println!("Other message received"),
+            }
+        }
+    }
+
+    fn unsubscribe(&self) {
+
+    }
+
+    fn parse_data(&self) {
+
+    }
+}
+
+pub struct UpbitBook {
+    pub name: String,
+    pub base_ws: String,
+    pub channel: String,
+    pub codes: Vec<String>,
+}
+
+impl ExchangeFunctionality for UpbitBook{
+    fn subscribe(&self) {
+        let mut websocket = ClientBuilder::new(&self.base_ws)
+        .unwrap()
+        .connect(None)
+        .expect("Failed to connect to websocket");
+
+        let mut rng = thread_rng();
+        let random_id: String = (0..6).map(|_| rng.sample(Alphanumeric) as char).collect();
+
+        let message = json!(
+        [{
+                    "ticket": random_id,
+                }, {
+                    "type": &self.channel,
+                    "codes": &self.codes,
+                    "isOnlyRealtime": true,
+                }]
+        );
+
+        websocket.send_message(&OwnedMessage::Text(message.to_string()))
+            .expect("Failed to send message");
+
+        loop {
+            let message = match websocket.recv_message() {
+                Ok(message) => message,
+                Err(e) => {
+                    println!("Error receiving message: {:?}", e);
+                    break;
+                }
+            };
+
+            match message {
+                OwnedMessage::Text(s) => println!("Text message received: {}", s),
+                OwnedMessage::Binary(b) => println!("Binary message received: {:?} \n From binary to string: {:?}", b, String::from_utf8(b.clone())),
+                OwnedMessage::Close(_) => {
+                    println!("Closing connection");
+                    break;
+                }
+                _ => println!("Other message received"),
+            }
+        }
+}
+
+    fn unsubscribe(&self) {
+        // TODO
+    }
+
+    fn parse_data(&self) {
+        // TODO
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
