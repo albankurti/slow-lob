1b4e56f3940156ce566cc96b2e1c9de9bd816e9d
Alban Kurti <>
2023-02-02 23:21:52 +0100

    merge two sorted arrays, covnert vec of str to vec of f64

---
+++ src/utils.rs
@@ -0,0 +1,44 @@
+pub fn merge(vec1: Vec<usize>, vec2: Vec<usize>) -> Vec<usize> {
+    let mut vec: Vec<usize> = Vec::with_capacity(vec1.len() + vec2.len());
+
+    let mut a_pos: usize = 0;
+    let mut b_pos: usize = 0;
+    for _i in 0..vec.capacity(){
+        if a_pos >= vec1.len(){
+            vec.extend(&vec2[b_pos..vec2.len()]);
+            break;
+        }
+        if b_pos >= vec2.len(){
+            vec.extend(&vec1[a_pos..vec1.len()]);
+            break;
+        }
+        if vec1[a_pos] <= vec2[b_pos] {
+            vec.push(vec1[a_pos]);
+            a_pos += 1;
+        }
+        else{
+            vec.push(vec2[b_pos]);
+            b_pos += 1;
+        }
+    }
+    vec
+}
+
+pub fn convert_to_float(vec: Vec<Vec<String>>) -> Result<Vec<Vec<f64>>, String> {
+    let result = vec
+        .into_iter()
+        .map(|bid| {
+            bid.into_iter()
+                .map(|s: String| match s.parse::<f64>() {
+                    Ok(value) => Ok(value),
+                    Err(err) => return Err(format!("Error parsing float: {}", err)),
+                })
+                .collect::<Result<Vec<_>, _>>()
+        })
+        .collect::<Result<Vec<_>, _>>();
+
+    match result {
+        Ok(values) => Ok(values),
+        Err(err) => Err(err.to_string()),
+    }
+}
